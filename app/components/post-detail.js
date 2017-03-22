a3da33c806e16ef5856695b0f9153e5849d6b36d
Dylan Stackhouse and Jennifer Beem <dylan@>
2017-03-22 11:41:26 -0700

    add delete functionality to post-detail component

--- app/components/post-detail.js
+++ app/components/post-detail.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    destroyPost1(post) {
+      if (confirm('Are you super sure you want to remove negativity from the world?')) {
+        this.sendAction("destroyPost2", post);
+      }
+    }
+  }
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
+  },
+
+  actions: {
+    destroyPost3(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
